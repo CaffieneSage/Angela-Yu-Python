77dc24989837265fdabca265b49009faf789bbe6
TCS <caffeinesage44@gmail.com>
2023-01-23 11:47:55 +0000

    Refactored code into more readable functions and added extra steps

--- blackjack.py
+++ blackjack.py
@@ -6,30 +6,36 @@ def deal_cards():
     card = cards[number]
     return card
 
-card1 = deal_cards()
-card2 = deal_cards()
-total = card1 + card2
-print(card1)
-print(card2)
-print(f"The total is: {total}")
-print("do you want another card? y/n")
-choice = input()
-if choice == 'y':
-    card3 = deal_cards()
-    total3 = card1 + card2 + card3
-    print(card1, card2, card3, total3)
+def show2(a, b):
+    total = a + b
+    print(f"Your first card is {a}, your second card is  {b}.")
+    print(f"Your current total is {total}")
+
+
+def play():
+    card1 = deal_cards()
+    card2 = deal_cards()
+    show2(card1, card2)
+    print("do you want another card? y/n")
+    choice = input()
+    if choice == 'y':
+        card3 = deal_cards()
+        total3 = card1 + card2 + card3
+        print(card1, card2, card3, total3)
     if total3 > 21 or total3 < 17:
         print("You lose")
     elif total3 >= 17 or total3 <= 20:
         print("you win")
-elif choice == 'n':
-    total2 = card1 + card2
-    print(card1, card2, total2)
-    if total2 < 17:
-        print("You loose.")
-    elif total2 > 21:
-        print("You loose.")
-    elif total2 >= 17 or total2 <= 21:
-        print("You win!")
-else:
-    print("invalid input")
+
+
+def start():
+    print("Welcome, do you want to play blackjack? y/n")
+    choice1 = input()
+    if choice1 == 'y':
+        play()
+    elif choice1 == 'n':
+        print("See you next time!")
+    else:
+        print("Thats not right.")
+
+start()
